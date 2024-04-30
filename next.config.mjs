b74eedc093db8c7bb94ab26612108919da104600
Guga <gugakobakhidze18@gmail.com>
2024-04-30 22:38:11 +0400

    bugfix/changes
    changing middleware

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    nextIntl: {
-        locales: ['ge', 'en'],
-        defaultLocale: 'ge',
-        localeDetection: false
-      },
-};
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
